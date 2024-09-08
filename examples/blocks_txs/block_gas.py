b188ed1c73b9e8c409ff36e60ef6fe417cd96de5
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-09-08 12:24:23 -0400

    feat: add block query

---
+++ examples/blocks_txs/block_gas.py
@@ -0,0 +1,21 @@
+import asyncio
+import polars as pl
+
+from hypermanager.manager import HyperManager
+
+hypersync_client: str = "https://eth.hypersync.xyz"
+
+
+async def get_events():
+    manager = HyperManager(url=hypersync_client)
+
+    df: pl.DataFrame = await manager.get_blocks(block_range=1000)
+
+    print(df.head(5))
+
+    print(f"schema: {df.schema}")
+    print(df.select("base_fee_per_gas").head(5))
+
+
+if __name__ == "__main__":
+    asyncio.run(get_events())
