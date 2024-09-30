a4dab0ee3084a14e483a3c393ff8203027821824
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-09-30 11:12:48 -0400

    feat: add fixed block range to example

--- examples/blocks_txs/blocks.py
+++ examples/blocks_txs/blocks.py
@@ -9,12 +9,16 @@ hypersync_client: str = "https://eth.hypersync.xyz"
 async def get_events():
     manager = HyperManager(url=hypersync_client)
 
-    df: pl.DataFrame = await manager.get_blocks(block_range=100)
+    df: pl.DataFrame = await manager.get_blocks(
+        from_block=20050000, to_block=20160000, save_data=True
+    )
 
-    print(df.head(5))
+    df.write_parquet("blocks.parquet")
 
-    print(f"schema: {df.schema}")
-    print(df.select("base_fee_per_gas").head(5))
+    # print(df.head(5))
+
+    # print(f"schema: {df.schema}")
+    # print(df.select("base_fee_per_gas").head(5))
 
 
 if __name__ == "__main__":
--- examples/blocks_txs/txs.py
+++ examples/blocks_txs/txs.py
@@ -7,14 +7,14 @@ hypersync_client: str = "https://eth.hypersync.xyz"
 
 
 async def get_events():
-    manager = HyperManager(url=hypersync_client)
+    manager: HyperManager = HyperManager(url=hypersync_client)
 
-    df: pl.DataFrame = await manager.get_txs(block_range=100)
-
-    print(df.head(5))
-
-    print(f"schema: {df.schema}")
-    print(df.select("base_fee_per_gas").head(5))
+    # await manager.get_txs(from_block=20129500, to_block=20160000, save_data=True)
+    df: pl.DataFrame = await manager.get_txs(from_block=20133000, to_block=20150000)
+    print(df.schema)
+    df.select("block_number", "extra_data").unique().write_parquet(
+        "tx_extra_data.parquet"
+    )
 
 
 if __name__ == "__main__":
