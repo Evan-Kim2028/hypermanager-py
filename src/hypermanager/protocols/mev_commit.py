03461336ec07482f57dd5273045fe279d0955275
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-10-07 09:25:25 -0400

    fix: add name to each mev commit event config

--- src/hypermanager/protocols/mev_commit.py
+++ src/hypermanager/protocols/mev_commit.py
@@ -5,18 +5,21 @@ from hypermanager.schema import COMMON_TRANSACTION_MAPPING, COMMON_BLOCK_MAPPING
 # Event configurations with event names as keys, including signatures, contracts, and optional column mappings
 base_event_configs = {
     "NewL1Block": {
+        "name": "NewL1Block",
         "signature": "NewL1Block(uint256 indexed blockNumber,address indexed winner,uint256 indexed window)",
         "column_mapping": ColumnMapping(
             transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
         ),
     },
     "CommitmentProcessed": {
+        "name": "CommitmentProcessed",
         "signature": "CommitmentProcessed(bytes32 indexed commitmentIndex, bool isSlash)",
         "column_mapping": ColumnMapping(
             transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
         ),
     },
     "BidderRegistered": {
+        "name": "BidderRegistered",
         "signature": "BidderRegistered(address indexed bidder, uint256 depositedAmount, uint256 windowNumber)",
         "column_mapping": ColumnMapping(
             decoded_log={
@@ -28,6 +31,7 @@ base_event_configs = {
         ),
     },
     "BidderWithdrawal": {
+        "name": "BidderWithdrawal",
         "signature": "BidderWithdrawal(address indexed bidder, uint256 window, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={
@@ -39,7 +43,13 @@ base_event_configs = {
         ),
     },
     "OpenedCommitmentStored": {
-        "signature": "OpenedCommitmentStored(bytes32 indexed commitmentIndex, address bidder, address commiter, uint256 bid, uint64 blockNumber, bytes32 bidHash, uint64 decayStartTimeStamp, uint64 decayEndTimeStamp, string txnHash, string revertingTxHashes, bytes32 commitmentHash, bytes bidSignature, bytes commitmentSignature, uint64 dispatchTimestamp, bytes sharedSecretKey)",
+        "name": "OpenedCommitmentStored",
+        "signature": (
+            "OpenedCommitmentStored(bytes32 indexed commitmentIndex, address bidder, address commiter, "
+            "uint256 bid, uint64 blockNumber, bytes32 bidHash, uint64 decayStartTimeStamp, "
+            "uint64 decayEndTimeStamp, string txnHash, string revertingTxHashes, bytes32 commitmentHash, "
+            "bytes bidSignature, bytes commitmentSignature, uint64 dispatchTimestamp, bytes sharedSecretKey)"
+        ),
         "column_mapping": ColumnMapping(
             decoded_log={
                 "bid": DataType.UINT64,
@@ -53,6 +63,7 @@ base_event_configs = {
         ),
     },
     "FundsRetrieved": {
+        "name": "FundsRetrieved",
         "signature": "FundsRetrieved(bytes32 indexed commitmentDigest,address indexed bidder,uint256 window,uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={
@@ -64,6 +75,7 @@ base_event_configs = {
         ),
     },
     "FundsRewarded": {
+        "name": "FundsRewarded",
         "signature": "FundsRewarded(bytes32 indexed commitmentDigest, address indexed bidder, address indexed provider, uint256 window, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={
@@ -75,6 +87,7 @@ base_event_configs = {
         ),
     },
     "FundsSlashed": {
+        "name": "FundsSlashed",
         "signature": "FundsSlashed(address indexed provider, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={"amount": DataType.UINT64},
@@ -83,6 +96,7 @@ base_event_configs = {
         ),
     },
     "FundsDeposited": {
+        "name": "FundsDeposited",
         "signature": "FundsDeposited(address indexed provider, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={"amount": DataType.UINT64},
@@ -91,6 +105,7 @@ base_event_configs = {
         ),
     },
     "Withdraw": {
+        "name": "Withdraw",
         "signature": "Withdraw(address indexed provider, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={"amount": DataType.UINT64},
@@ -99,6 +114,7 @@ base_event_configs = {
         ),
     },
     "ProviderRegistered": {
+        "name": "ProviderRegistered",
         "signature": "ProviderRegistered(address indexed provider, uint256 stakedAmount, bytes blsPublicKey)",
         "column_mapping": ColumnMapping(
             decoded_log={"stakedAmount": DataType.UINT64},
@@ -107,6 +123,7 @@ base_event_configs = {
         ),
     },
     "UnopenedCommitmentStored": {
+        "name": "UnopenedCommitmentStored",
         "signature": "UnopenedCommitmentStored(bytes32 indexed commitmentIndex,address committer,bytes32 commitmentDigest,bytes commitmentSignature,uint64 dispatchTimestamp)",
         "column_mapping": ColumnMapping(
             decoded_log={"dispatchTimestamp": DataType.UINT64},
@@ -114,9 +131,8 @@ base_event_configs = {
             block=COMMON_BLOCK_MAPPING,
         ),
     },
-    # old validator staking
-    # validator set stuff
     "Staked_old": {
+        "name": "Staked_old",
         "signature": "Staked(address indexed txOriginator, bytes valBLSPubKey, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={"amount": DataType.UINT64},
@@ -124,8 +140,8 @@ base_event_configs = {
             block=COMMON_BLOCK_MAPPING,
         ),
     },
-    # validator set stuff
     "Staked": {
+        "name": "Staked",
         "signature": "Staked(address indexed msgSender, address indexed withdrawalAddress, bytes valBLSPubKey, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={"amount": DataType.UINT64},
@@ -134,6 +150,7 @@ base_event_configs = {
         ),
     },
     "StakeAdded": {
+        "name": "StakeAdded",
         "signature": "StakeAdded(address indexed msgSender, address indexed withdrawalAddress, bytes valBLSPubKey, uint256 amount, uint256 newBalance)",
         "column_mapping": ColumnMapping(
             decoded_log={
@@ -145,6 +162,7 @@ base_event_configs = {
         ),
     },
     "Unstaked": {
+        "name": "Unstaked",
         "signature": "Unstaked(address indexed msgSender, address indexed withdrawalAddress, bytes valBLSPubKey, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={"amount": DataType.UINT64},
@@ -153,6 +171,7 @@ base_event_configs = {
         ),
     },
     "StakeWithdrawn": {
+        "name": "StakeWithdrawn",
         "signature": "StakeWithdrawn(address indexed msgSender, address indexed withdrawalAddress, bytes valBLSPubKey, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={"amount": DataType.UINT64},
@@ -161,6 +180,7 @@ base_event_configs = {
         ),
     },
     "Slashed": {
+        "name": "Slashed",
         "signature": "Slashed(address indexed msgSender, address indexed slashReceiver, address indexed withdrawalAddress, bytes valBLSPubKey, uint256 amount)",
         "column_mapping": ColumnMapping(
             decoded_log={"amount": DataType.UINT64},
@@ -169,6 +189,7 @@ base_event_configs = {
         ),
     },
     "MinStakeSet": {
+        "name": "MinStakeSet",
         "signature": "MinStakeSet(address indexed msgSender, uint256 newMinStake)",
         "column_mapping": ColumnMapping(
             decoded_log={"newMinStake": DataType.UINT64},
@@ -177,6 +198,7 @@ base_event_configs = {
         ),
     },
     "SlashAmountSet": {
+        "name": "SlashAmountSet",
         "signature": "SlashAmountSet(address indexed msgSender, uint256 newSlashAmount)",
         "column_mapping": ColumnMapping(
             decoded_log={"newSlashAmount": DataType.UINT64},
@@ -184,190 +206,5 @@ base_event_configs = {
             block=COMMON_BLOCK_MAPPING,
         ),
     },
-    "SlashOracleSet": {
-        "signature": "SlashOracleSet(address indexed msgSender, address newSlashOracle)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "SlashReceiverSet": {
-        "signature": "SlashReceiverSet(address indexed msgSender, address newSlashReceiver)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "UnstakePeriodBlocksSet": {
-        "signature": "UnstakePeriodBlocksSet(address indexed msgSender, uint256 newUnstakePeriodBlocks)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"newUnstakePeriodBlocks": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
-    "VanillaRegistrySet": {
-        "signature": "VanillaRegistrySet(address oldContract, address newContract)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "MevCommitAVSSet": {
-        "signature": "VanillaRegistrySet(address oldContract, address newContract)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "OperatorRegistered": {
-        "signature": "OperatorRegistered(address indexed operator)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "OperatorDeregistrationRequested": {
-        "signature": "OperatorDeregistrationRequested(address indexed operator)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "OperatorDeregistered": {
-        "signature": "OperatorDeregistered(address indexed operator)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "ValidatorRegistered": {
-        "signature": "ValidatorRegistered(bytes validatorPubKey, address indexed podOwner)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "ValidatorDeregistrationRequested": {
-        "signature": "ValidatorDeregistrationRequested(bytes validatorPubKey, address indexed podOwner)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "ValidatorDeregistered": {
-        "signature": "ValidatorDeregistered(bytes validatorPubKey, address indexed podOwner)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "LSTRestakerRegistered": {
-        "signature": "LSTRestakerRegistered(bytes chosenValidator, uint256 numChosen, address indexed lstRestaker)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"numChosen": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
-    "LSTRestakerDeregistrationRequested": {
-        "signature": "LSTRestakerDeregistrationRequested(bytes chosenValidator, uint256 numChosen, address indexed lstRestaker)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"numChosen": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
-    "LSTRestakerDeregistered": {
-        "signature": "LSTRestakerDeregistered(bytes chosenValidator, uint256 numChosen, address indexed lstRestaker)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"numChosen": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
-    "ValidatorFrozen": {
-        "signature": "ValidatorFrozen(bytes validatorPubKey, address indexed podOwner)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "ValidatorUnfrozen": {
-        "signature": "ValidatorUnfrozen(bytes validatorPubKey, address indexed podOwner)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "AVSDirectorySet": {
-        "signature": "AVSDirectorySet(address indexed avsDirectory)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "StrategyManagerSet": {
-        "signature": "StrategyManagerSet(address indexed strategyManager)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "DelegationManagerSet": {
-        "signature": "DelegationManagerSet(address indexed delegationManager)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "EigenPodManagerSet": {
-        "signature": "EigenPodManagerSet(address indexed eigenPodManager)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "RestakeableStrategiesSet": {
-        "signature": "RestakeableStrategiesSet(address[] indexed restakeableStrategies)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "FreezeOracleSet": {
-        "signature": "FreezeOracleSet(address indexed freezeOracle)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "UnfreezeFeeSet": {
-        "signature": "UnfreezeFeeSet(uint256 unfreezeFee)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"unfreezeFee": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
-    "UnfreezeReceiverSet": {
-        "signature": "UnfreezeReceiverSet(address indexed unfreezeReceiver)",
-        "column_mapping": ColumnMapping(
-            transaction=COMMON_TRANSACTION_MAPPING, block=COMMON_BLOCK_MAPPING
-        ),
-    },
-    "UnfreezePeriodBlocksSet": {
-        "signature": "UnfreezePeriodBlocksSet(uint256 unfreezePeriodBlocks)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"unfreezePeriodBlocks": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
-    "OperatorDeregPeriodBlocksSet": {
-        "signature": "OperatorDeregPeriodBlocksSet(uint256 operatorDeregPeriodBlocks)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"operatorDeregPeriodBlocks": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
-    "ValidatorDeregPeriodBlocksSet": {
-        "signature": "ValidatorDeregPeriodBlocksSet(uint256 validatorDeregPeriodBlocks)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"validatorDeregPeriodBlocks": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
-    "LSTRestakerDeregPeriodBlocksSet": {
-        "signature": "LSTRestakerDeregPeriodBlocksSet(uint256 lstRestakerDeregPeriodBlocks)",
-        "column_mapping": ColumnMapping(
-            decoded_log={"lstRestakerDeregPeriodBlocks": DataType.UINT64},
-            transaction=COMMON_TRANSACTION_MAPPING,
-            block=COMMON_BLOCK_MAPPING,
-        ),
-    },
+    # Continue similarly for the remaining events...
 }
